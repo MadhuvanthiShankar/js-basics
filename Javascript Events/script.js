befbf8a0621771e885685ba50127833d647ebae2
Madhu <madhuvanthishankar@gmail.com>
2024-05-13 17:46:35 +0530

    js events

---
+++ Javascript Events/script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("click", ()=>{
+    alert("yay! I was clicked")
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(e)
+})
+
+//context menu
+//dblclick
+//keyboard events
+
+//below is the list of events
+//https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
