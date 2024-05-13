fc29f0b6856355b2218d067f15f39993029dc5d6
Madhu <madhuvanthishankar@gmail.com>
2024-05-13 17:44:16 +0530

    dom manipulation

---
+++ DOM Manip/script.js
@@ -0,0 +1,3 @@
+document.getElementById("insideBox").style.backgroundColor = "red"
+// document.getElementsByClassName("container").firstElementChild.style.backgroundColor = "green"
+console.log(document.getElementsByClassName("box").firstElement)
