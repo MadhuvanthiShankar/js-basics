3dc145cf0f0439e014e61fdb1b0616ea5d70a0a3
Madhu <madhuvanthishankar@gmail.com>
2024-05-13 17:43:39 +0530

    main

---
+++ index.js
@@ -0,0 +1,15 @@
+// function nice(name){
+//     console.log(name + " is 20");
+//     console.log(name + " is Indian");
+//     console.log(name + " is SRMITE");
+//     console.log(name + " is Nice");
+// }
+
+// nice("Manthra")
+
+const sum = (a, b, c) => {
+    return a + b + c;
+}
+
+let y = sum(1, 2, 3)
+console.log(y)
